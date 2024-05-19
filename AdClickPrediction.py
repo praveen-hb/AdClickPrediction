4d30dc76565e4e9f79df3d89efa6d33527b66ff5
Praveen Kumar Reddy L <59311248+praveen-hb@users.noreply.github.com>
2024-05-19 21:44:10 +0530

    Update AdClickPrediction.py

--- AdClickPrediction.py
+++ AdClickPrediction.py
@@ -1,8 +1,6 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd 
 
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
 
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
